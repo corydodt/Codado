e6b730167c3879d6c7300dd8b1730776bd8d5910
Cory Dodt <corydodt@gmail.com>
2017-11-06 15:32:51 -0800

    0.4.994 ugh

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
-from setuptools import setup
+from inspect import cleandoc
 
-from pip.req import parse_requirements
+from setuptools import setup
 
 from codado import _version
 
 
-reqs = parse_requirements('requirements.txt')
-
 setup(
-  name = 'Codado',
-  packages = ['codado', 'codado.kleinish'],
-  version = _version.__version__,
-  description = 'A collection of system development utilities',
-  author = 'Cory Dodt',
-  author_email = 'corydodt@gmail.com',
-  url = 'https://github.com/corydodt/Codado',
-  keywords = ['twisted', 'utility'],
-  classifiers = [],
-  scripts = ['bin/urltool', 'bin/jentemplate'],
-  install_requires=list(reqs)
+    name = 'Codado',
+    packages = ['codado', 'codado.kleinish'],
+    version = _version.__version__,
+    description = 'A collection of system development utilities',
+    author = 'Cory Dodt',
+    author_email = 'corydodt@gmail.com',
+    url = 'https://github.com/corydodt/Codado',
+    keywords = ['twisted', 'utility'],
+    classifiers = [],
+    scripts = ['bin/urltool', 'bin/jentemplate'],
+    install_requires=cleandoc('''
+        attrs
+        ftfy==4.4.3
+        jinja2
+        pyyaml
+        werkzeug
+        ''').split()
 )
